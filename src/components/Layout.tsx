2ffa0fa2bd9ed2f7ffdfd9404579b8536bff7a5d
IAMISTP <hyeminpak@gmail.com>
2023-12-31 18:07:31 +0900

    footer 컴포넌트 적용

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,17 @@
 import React, { ReactNode } from "react";
+import MenuList from "./Menu";
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 const Layout = ({ children }: LayoutProps) => {
-  return <div className="layout">{children}</div>;
+  return (
+    <div className="layout">
+      {children}
+      <MenuList />
+    </div>
+  );
 };
 
 export default Layout;
