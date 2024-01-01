b7723e4ea08d26a8df4aab4729797c9801e80348
IAMISTP <hyeminpak@gmail.com>
2024-01-01 23:57:46 +0900

    파이어베이스 실시간 사용자 상태 확인 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,31 @@
 import Router from "components/Router";
 import Layout from "components/Layout";
-import { getAuth } from "firebase/auth";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { app } from "firebaseApp";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   const auth = getAuth(app);
+  const [init, setInit] = useState<boolean>(false);
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(!!auth?.currentUser);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsAuthenticated(true);
+      } else {
+        setIsAuthenticated(false);
+      }
+      setInit(true);
+    });
+  }, [auth]);
 
   return (
     <Layout>
-      <Router />
+      <ToastContainer />
+      {init ? <Router isAuthenticated={isAuthenticated} /> : "loading"}
     </Layout>
   );
 }
