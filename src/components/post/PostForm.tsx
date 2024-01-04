3a55fe8f61cdb53aab6e3857f0caad6484589de1
IAMISTP <hyeminpak@gmail.com>
2024-01-04 17:07:09 +0900

    게시글 쓰기 구현

--- src/components/post/PostForm.tsx
+++ src/components/post/PostForm.tsx
@@ -1,16 +1,50 @@
-import React from "react";
+import AuthContext from "context/AuthContext";
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "firebaseApp";
+import React, { ChangeEvent, FormEvent, useContext, useState } from "react";
 import { FiImage } from "react-icons/fi";
+import { toast } from "react-toastify";
 
 const PostForm = () => {
+  const [content, setContent] = useState("");
+  const { user } = useContext(AuthContext);
+
   const handleFileUpload = () => {};
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      await addDoc(collection(db, "post"), {
+        content: content,
+        createdAt: new Date()?.toLocaleDateString("ko", {
+          hour: "2-digit",
+          minute: "2-digit",
+          second: "2-digit",
+        }),
+        uid: user?.uid,
+        email: user?.email,
+      });
+      setContent("");
+      toast.success("게시글을 생성하였습니다.");
+    } catch (e: any) {
+      toast.error(e.message);
+    }
+  };
+  const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    if (name === "content") {
+      setContent(value);
+    }
+  };
   return (
-    <form className="post-form">
+    <form className="post-form" onSubmit={handleSubmit}>
       <textarea
         className="post-form__textarea"
         required
         name="content"
         id="content"
         placeholder="What is happening?"
+        onChange={handleChange}
+        value={content}
       />
       <div className="post-form__submit-area">
         <label htmlFor="file-input" className="post-form__file">
