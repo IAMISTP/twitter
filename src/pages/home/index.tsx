37640144469c28fc7e165373f2ac11a8ddb2b3be
IAMISTP <hyeminpak@gmail.com>
2024-01-04 16:39:55 +0900

    상단 메뉴 스타일 추가

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -61,10 +61,12 @@ const posts: PostsProps[] = [
 const HomePage = () => {
   return (
     <div className="home">
-      <div className="home__title">Home</div>
-      <div className="home__tabs">
-        <div className="home__tab home__tab__active">For you</div>
-        <div className="home__tab">Following</div>
+      <div className="home__top">
+        <div className="home__title">Home</div>
+        <div className="home__tabs">
+          <div className="home__tab home__tab__active">For you</div>
+          <div className="home__tab">Following</div>
+        </div>
       </div>
       <PostForm />
 
